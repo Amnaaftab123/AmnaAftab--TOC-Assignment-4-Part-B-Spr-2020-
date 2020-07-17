4c82cd17d9641e78e5e32a58569d47b3f0ca311d
Amnaaftab123 <65035812+Amnaaftab123@users.noreply.github.com>
2020-07-17 13:48:25 +0500

    completed
    ..T

---
+++ AmnaAftab_Assignment04_PartB/Assets/Scripts/SplashController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    public void GotoPenguinScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void GotoHummingBirdScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
+
