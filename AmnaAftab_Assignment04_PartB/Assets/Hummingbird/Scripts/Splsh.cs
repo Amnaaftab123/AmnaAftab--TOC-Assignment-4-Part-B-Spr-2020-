2808d2bdc5b52ff86af73b1dff426db58a67cbb7
Amnaaftab123 <65035812+Amnaaftab123@users.noreply.github.com>
2020-07-23 23:57:02 +0500

    asset

---
+++ AmnaAftab_Assignment04_PartB/Assets/Hummingbird/Scripts/Splsh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splsh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("load", 2);
+    }
+
+    void load()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
